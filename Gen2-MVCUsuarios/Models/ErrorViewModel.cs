21bc2ff7821124ce7915833240bff26173fea17f
Cotze <carlosrodrigoatl@gmail.com>
2022-02-15 16:20:29 -0600

    first commit

---
+++ Gen2-MVCUsuarios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gen2_MVCUsuarios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
